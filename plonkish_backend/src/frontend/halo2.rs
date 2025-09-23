49a7aefe9f5dcf6dfbeba1e1bc9ee0ec20084b4b
DoHoonKim <dohoon1097819@gmail.com>
2025-09-23 21:08:21 +0900

    Set lookup argument to CS properly

--- plonkish_backend/src/frontend/halo2.rs
+++ plonkish_backend/src/frontend/halo2.rs
@@ -60,6 +60,8 @@ impl<F: Field, C: CircuitExt<F>> Halo2Circuit<F, C> {
             (cs, config)
         };
         let constants = cs.constants().clone();
+        // for mv-lookup compatibility
+        let cs = cs.chunk_lookups();
 
         let num_witness_polys = num_by_phase(&cs.advice_column_phase());
         let challenge_idx = idx_order_by_phase(&cs.challenge_phase(), 0);
